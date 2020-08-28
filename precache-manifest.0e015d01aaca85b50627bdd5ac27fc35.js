67317be2a87cca88627cd919282b21a15a57e664
kunjan tanna <kvtanna.om@gmail.com>
2020-08-28 17:53:13 +0530

    Updates

---
+++ precache-manifest.0e015d01aaca85b50627bdd5ac27fc35.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b55de733e9625b7856cd400d71e94cd",
+    "url": "/cms/index.html"
+  },
+  {
+    "revision": "6939dd223d4d38d800c6",
+    "url": "/cms/static/css/10.938c68d3.chunk.css"
+  },
+  {
+    "revision": "36ab92853d4b27f9a6b5",
+    "url": "/cms/static/css/11.5ed4cae5.chunk.css"
+  },
+  {
+    "revision": "ce635e8a0fd12979c334",
+    "url": "/cms/static/css/4.9d9e154e.chunk.css"
+  },
+  {
+    "revision": "980288bfbd92915c2542",
+    "url": "/cms/static/css/6.5b7d99f3.chunk.css"
+  },
+  {
+    "revision": "ad4f4c5c1d4cb406d056",
+    "url": "/cms/static/css/7.938c68d3.chunk.css"
+  },
+  {
+    "revision": "641beaa6c1af9773a457",
+    "url": "/cms/static/css/8.938c68d3.chunk.css"
+  },
+  {
+    "revision": "8b0e2da1f1bd43c4d3cb",
+    "url": "/cms/static/css/9.938c68d3.chunk.css"
+  },
+  {
+    "revision": "29a9761a60e2a025b37c",
+    "url": "/cms/static/css/main.92f2093b.chunk.css"
+  },
+  {
+    "revision": "d0ee83a016d7b713f621",
+    "url": "/cms/static/js/0.2d68e23c.chunk.js"
+  },
+  {
+    "revision": "2f48cc92c9b49cca1eae",
+    "url": "/cms/static/js/1.0b36f606.chunk.js"
+  },
+  {
+    "revision": "d51c699b4357cb37dcc86d11129f128d",
+    "url": "/cms/static/js/1.0b36f606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6939dd223d4d38d800c6",
+    "url": "/cms/static/js/10.7939bd32.chunk.js"
+  },
+  {
+    "revision": "36ab92853d4b27f9a6b5",
+    "url": "/cms/static/js/11.44f8ecee.chunk.js"
+  },
+  {
+    "revision": "24c8d99c6ef59a680ce4",
+    "url": "/cms/static/js/12.ab1a2926.chunk.js"
+  },
+  {
+    "revision": "58beb84fe186953d8775",
+    "url": "/cms/static/js/13.72a3d1ec.chunk.js"
+  },
+  {
+    "revision": "618b2f6d0e6230e58200",
+    "url": "/cms/static/js/14.3700d61e.chunk.js"
+  },
+  {
+    "revision": "ef0de6a62c1a40c4c689",
+    "url": "/cms/static/js/15.f4fc78a6.chunk.js"
+  },
+  {
+    "revision": "8794cc7fe6194e5d4ff2",
+    "url": "/cms/static/js/16.619fadb8.chunk.js"
+  },
+  {
+    "revision": "36fe9d4a069b202084a6",
+    "url": "/cms/static/js/17.aec7a59b.chunk.js"
+  },
+  {
+    "revision": "f2fadad424c19e693f88",
+    "url": "/cms/static/js/18.3e8d3b93.chunk.js"
+  },
+  {
+    "revision": "ce635e8a0fd12979c334",
+    "url": "/cms/static/js/4.405b1760.chunk.js"
+  },
+  {
+    "revision": "4d77f9d405bc1efe97c85ea8ee982958",
+    "url": "/cms/static/js/4.405b1760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1443ef2246abd13ac54",
+    "url": "/cms/static/js/5.11cbefdc.chunk.js"
+  },
+  {
+    "revision": "980288bfbd92915c2542",
+    "url": "/cms/static/js/6.a621c562.chunk.js"
+  },
+  {
+    "revision": "ad4f4c5c1d4cb406d056",
+    "url": "/cms/static/js/7.6b2c609f.chunk.js"
+  },
+  {
+    "revision": "641beaa6c1af9773a457",
+    "url": "/cms/static/js/8.4b1431b5.chunk.js"
+  },
+  {
+    "revision": "8b0e2da1f1bd43c4d3cb",
+    "url": "/cms/static/js/9.4cac079c.chunk.js"
+  },
+  {
+    "revision": "29a9761a60e2a025b37c",
+    "url": "/cms/static/js/main.4a62da35.chunk.js"
+  },
+  {
+    "revision": "b7d9ef1b4cb59dac0cf9",
+    "url": "/cms/static/js/runtime-main.213f5d6c.js"
+  },
+  {
+    "revision": "b0220bed0dd272002ea4f0a2d00c09ae",
+    "url": "/cms/static/media/cms-logo.b0220bed.png"
+  },
+  {
+    "revision": "c8c9986d072d0fd1e200675094f8542b",
+    "url": "/cms/static/media/content-img-4.c8c9986d.jpg"
+  },
+  {
+    "revision": "3d2fa2e544004aef2ea641698b715af6",
+    "url": "/cms/static/media/feather.3d2fa2e5.woff"
+  },
+  {
+    "revision": "6a750f0119ba9c8ab8a994a4796599a6",
+    "url": "/cms/static/media/feather.6a750f01.ttf"
+  },
+  {
+    "revision": "931c9e74d0b20947054bfe6a5b74a838",
+    "url": "/cms/static/media/feather.931c9e74.eot"
+  },
+  {
+    "revision": "b23df4db71a29dbb733a0e555a7db8f9",
+    "url": "/cms/static/media/feather.b23df4db.svg"
+  },
+  {
+    "revision": "63f5a96a010605a3708c757d6c28f26f",
+    "url": "/cms/static/media/forgot-password.63f5a96a.png"
+  },
+  {
+    "revision": "222e25f53e8f2a25ab93bb09f2b99e07",
+    "url": "/cms/static/media/google.222e25f5.svg"
+  },
+  {
+    "revision": "fd58a052054c690506118c4b7a0f731d",
+    "url": "/cms/static/media/login.fd58a052.png"
+  },
+  {
+    "revision": "303e7be21b2bf111547d4732fa0f2f6e",
+    "url": "/cms/static/media/logo-1.303e7be2.ico"
+  },
+  {
+    "revision": "23d1bae7ef4c0758e2b374755de5557e",
+    "url": "/cms/static/media/register.23d1bae7.jpg"
+  },
+  {
+    "revision": "eb4e894d949eb0ce8f21368b70f3dd3b",
+    "url": "/cms/static/media/vuesax-login-bg.eb4e894d.jpg"
+  }
+]);
---
+++ static/js/16.619fadb8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[16],{649:function(e,a,t){"use strict";t.r(a);var n=t(11),r=t(12),u=t(14),c=t(13),s=t(0),o=t.n(s),i=function(e){Object(u.a)(t,e);var a=Object(c.a)(t);function t(){return Object(n.a)(this,t),a.apply(this,arguments)}return Object(r.a)(t,[{key:"render",value:function(){return o.a.createElement("h4",null,"You're UserDashboard.")}}]),t}(o.a.Component);a.default=i}}]);
+//# sourceMappingURL=16.619fadb8.chunk.js.map
